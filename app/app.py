75de6ac49a40836572cb80c126476d013959c0a1
Merrik Gilbert <merrik.gilbert@realist.ai>
2026-02-11 21:39:16 -0700

    starting foundation for website

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
+import os
+import sys
+import secrets
+
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('home.html', year=datetime.now().year)
+
+@app.route('/connect')
+def contact():
+    return render_template('connect.html', year=datetime.now().year)
+
+@app.route('/about')
+def home():
+    return render_template('about-us.html', year=datetime.now().year)
+
+@app.route('/package')
+def contact():
+    return render_template('packages.html', year=datetime.now().year)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
